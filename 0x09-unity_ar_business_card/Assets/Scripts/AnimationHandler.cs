8d1adecbc83d85daedb45bb606b2c45be80dd445
eNobreg <ezra.nobrega@outlook.com>
2020-11-04 16:01:17 -0500

    Create initial project

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class AnimationHandler : MonoBehaviour
+{
+	public Animator gitHub;
+	private void Start()
+	{
+		gitHub = GetComponentInChildren<Animator>();
+	}
+
+	public void playAnimations()
+	{
+		Debug.Log("Tracked");
+		gitHub.SetTrigger("playAnimations");
+	}
+
+	public void stopAnimations()
+	{
+
+	}
+}
---
+++ 0x09-unity_ar_business_card/Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+
+	public void toURL(string url)
+	{
+		Application.OpenURL(url);
+	}
+}
